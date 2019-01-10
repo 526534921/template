43bdf95880360f439dd68c1263a6fc93b3d5f802
Twinkleee <365189883@qq.com>
2019-01-10 14:31:24 +0800

    Signed-off-by: Twinkleee <365189883@qq.com>

---
+++ cmd.cmd
@@ -0,0 +1,8 @@
+chcp 65001
+md banner
+md 信息与认可书
+git clone https://github.com/HakunaMatata052/template.git html
+rmdir /s/q html\.git
+set /a n=%random%%%18+1
+git clone -b m_temp_%n% https://github.com/HakunaMatata052/template_mobile.git wap/m_temp
+rmdir /s/q wap\m_temp\.git
